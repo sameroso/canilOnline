3b4261e3237176172ef93f19073ad1d2ced60829
sameroso <samerkkr@gmail.com>
2020-08-28 14:51:16 -0300

    client eslint config

---
+++ client/.eslintrc.js
@@ -0,0 +1,43 @@
+// eslint-disable-next-line no-undef
+module.exports = {
+  env: {
+    browser: true,
+    es2020: true,
+    jest: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:import/errors',
+    'plugin:import/warnings',
+    'prettier',
+  ],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  plugins: ['react', 'prettier', 'import'],
+  rules: {
+    'prettier/prettier': 'error',
+    'padding-line-between-statements': [
+      'error',
+      { blankLine: 'always', prev: '*', next: 'return' },
+      { blankLine: 'always', prev: ['const', 'let', 'var'], next: '*' },
+      { blankLine: 'always', prev: '*', next: ['const', 'let', 'var'] },
+      {
+        blankLine: 'any',
+        prev: ['const', 'let', 'var'],
+        next: ['const', 'let', 'var'],
+      },
+      { blankLine: 'always', prev: '*', next: 'block-like' },
+    ],
+    'import/named': 0,
+    'import/namespace': 0,
+    'import/newline-after-import': 2,
+    'no-alert': 1,
+  },
+  settings: { react: { version: 'detect' } },
+}
